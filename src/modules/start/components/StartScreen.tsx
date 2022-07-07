e091fca07b3bd24c0d2564649685a9977c9e0c5f
foobarsik <olgaraat@gmail.com>
2022-07-07 22:45:21 +0200

    feat: add logo and LoadingStateSwitcher to home screen

--- src/modules/start/components/StartScreen.tsx
+++ src/modules/start/components/StartScreen.tsx
@@ -2,19 +2,17 @@ import styled from '@emotion/native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import * as mobx from 'mobx';
-import {useTranslation} from 'react-i18next';
 import {useStores} from '../../common/stores/RootStore';
 import {Observer} from 'mobx-react-lite';
-import {ActivityIndicator, FlatList, ScrollView} from 'react-native';
+import {View, FlatList, ScrollView, Image} from 'react-native';
 import {VideoPreview} from '../../videos/components/VideoPreview';
 import {VideoCategory} from '../../videoCategories/components/VideoCategory';
 import {H2} from '../../../theme/Typography';
+import {LoadingStateSwitcher} from '../../common/components/LoadingStateSwitcher';
 
 export const StartScreen = () => {
-	const rootStore = useStores();
-	const videosStore = rootStore.videosStore;
-	const videoCategoriesStore = rootStore.videoCategoriesStore;
-	const {t} = useTranslation('general');
+	const {videosStore, videoCategoriesStore} = useStores();
+	const logoImage = require('../../../assets/logo.png');
 	let activeCategoryId: string | undefined;
 
 	const handleCategoryPress = (categoryId: string) => {
@@ -26,34 +24,40 @@ export const StartScreen = () => {
 		<Observer>
 			{() => (
 				<Box>
-					<ScrollView horizontal={true} showsHorizontalScrollIndicator={false} pagingEnabled={true}>
-						{videoCategoriesStore.categories.map(category => (
-							<VideoCategory
-								id={category.id}
-								title={category.title}
-								isActive={activeCategoryId === category.id}
-								key={category.id}
-								pressHandler={handleCategoryPress}
-							/>
-						))}
-					</ScrollView>
+					<View>
+						<Logo source={logoImage} resizeMode="contain" />
+						<ScrollView horizontal={true} showsHorizontalScrollIndicator={false} pagingEnabled={true}>
+							{videoCategoriesStore.categories.map(category => (
+								<VideoCategory
+									id={category.id}
+									title={category.title}
+									isActive={activeCategoryId === category.id}
+									key={category.id}
+									pressHandler={handleCategoryPress}
+								/>
+							))}
+						</ScrollView>
+					</View>
 
-					{videosStore.isLoading && <ActivityIndicator size="large" color="red" />}
+					<Videos>
+						<LoadingStateSwitcher
+							loadingState={videosStore.loadingState}
+							tryAgainCallback={() => videosStore.getVideos(activeCategoryId)}
+						/>
 
-					<InfoMessage>
-						{videosStore.fetchedResults} / {videosStore.totalResults}
-					</InfoMessage>
+						<InfoMessage>
+							{videosStore.fetchedResults} / {videosStore.totalResults}
+						</InfoMessage>
 
-					{videosStore.isError && <Error>{t('errorHappened')}</Error>}
-
-					<FlatList
-						data={mobx.toJS(videosStore.videos)}
-						renderItem={VideoPreview}
-						keyExtractor={video => video.id}
-						onEndReached={() => {
-							videosStore.getVideos(activeCategoryId);
-						}}
-					/>
+						<FlatList
+							data={mobx.toJS(videosStore.videos)}
+							renderItem={VideoPreview}
+							keyExtractor={video => video.id}
+							onEndReached={() => {
+								videosStore.getVideos(activeCategoryId);
+							}}
+						/>
+					</Videos>
 				</Box>
 			)}
 		</Observer>
@@ -64,9 +68,14 @@ const Box = styled(SafeAreaView)(({theme}) => ({
 	backgroundColor: theme.color.background,
 }));
 
-const Error = styled(H2)(() => ({
+const Videos = styled(View)(() => ({
+	height: '100%',
+}));
+
+const Logo = styled(Image)(() => ({
+	height: 30,
 	alignSelf: 'center',
-	color: 'red',
+	marginBottom: 10,
 }));
 
 const InfoMessage = styled(H2)(() => ({
